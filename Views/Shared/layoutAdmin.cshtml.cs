3f006105274585b51033140939046860e8f246bb
Umaima Gull <gullumaimagull@gmail.com>
2026-08-29 10:46:08 +0300

    Add files via upload

---
+++ Views/Shared/layoutAdmin.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Darsenizami.Views.Shared
+{
+    public class layoutAdminModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
